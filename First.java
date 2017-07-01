30fb12adc26d3830b5cb7d81505f99f1c44579fd
Sujan <wamiqy2k@gmail.com>
2017-07-01 23:46:04 +0530

    Updated code

--- First.java
+++ First.java
@@ -1,6 +1,7 @@
 public class First{
 	public static void main(String[] args){
 		System.out.println("Hello Docker !!!");
-		System.out.println("New Code !!!");
+		System.out.println("Modified Code inside container!!!");
+		
 	}
 }
